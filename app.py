a32db6ef74fba8e869d4a4460b3f52cc0ea03837
Aleks <windows02@internet.ru>
2025-02-11 02:29:45 +0400

    corrected for hosting

--- app.py
+++ app.py
@@ -213,4 +213,4 @@ if __name__ == '__main__':
     deletePopularProducts(app)
     deleteNews(app)
     addManager('Aleks', 'windows02@internet.ru', 'alexik020911')  
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
